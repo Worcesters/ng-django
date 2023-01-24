0971bdbc50e615f4073f38b86e70cae59057ab35
Worcesters <jeremy.didier77@gmail.com>
2023-01-24 15:19:50 +0100

    update fix and requierement

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ng-django.wsgi:application
--- djback/djback/settings.py
+++ djback/djback/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'user',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
 
@@ -77,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djback.urls'
