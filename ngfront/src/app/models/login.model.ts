d0c467bf31e74bd3d0cda5dafbc2fe23546a1dc7
Worcesters <jeremy.didier77@gmail.com>
2023-01-31 14:41:12 +0100

    login model

--- ngfront/src/app/models/login.model.ts
+++ ngfront/src/app/models/login.model.ts
@@ -1,3 +1,3 @@
 export type UserType = { username: string, password: string }
 export type UserApiType = { username: string, password: string }
-export const baseUrl: string = 'http://localhost:8000/api/';
+export const baseUrl: string = 'https://ng-django.vercel.app/api/';
