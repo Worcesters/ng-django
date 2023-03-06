1b2c243cb289e97efc44a2c2e2ab13b512b42de5
Worcesters <jeremy.didier77@gmail.com>
2023-03-06 22:43:31 +0100

    wip (publication): develop - suppresion des fichier docker

--- ngfront/src/app/models/login.model.ts
+++ ngfront/src/app/models/login.model.ts
@@ -1,3 +1,3 @@
 export type UserType = { username: string, password: string }
 export type UserApiType = { username: string, password: string }
-export const baseUrl: string = 'https://ng-django.vercel.app/api/';
+export const baseUrl: string = '127.0.0.1:8000/api/';
