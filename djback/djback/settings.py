b8ea292057d197b8bc8e8b26a68342bb812e3a28
PARTENORGROUP\jdidier <jdidier@partenorgroup.net>
2023-03-20 17:13:35 +0100

    updated

--- djback/djback/settings.py
+++ djback/djback/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-@j8)z8stx+g@^2-3l)@g=vw4rq8&gv$$%1k*m$n=rk*v&0_kz('
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = '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'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
