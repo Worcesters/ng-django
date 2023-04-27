bfeacaeb4543cd0c8bdca3d88021369597c970b8
PARTENORGROUP\jdidier <jdidier@partenorgroup.net>
2023-04-27 09:42:27 +0200

    feat (crashtest): develop - Ajout & modification liée a la chatbox

--- ngfront/src/app/components/crashtest/crashtest.component.ts
+++ ngfront/src/app/components/crashtest/crashtest.component.ts
@@ -6,17 +6,19 @@ import { ChatbotService } from 'src/app/services/chatbot.service';
   templateUrl: './crashtest.component.html',
   styleUrls: [ './crashtest.component.css' ]
 })
-
 export class CrashtestComponent {
-  val: string = ""
+  val: string = "";
   question: string = '';
   answer: string = '';
   messages: any[] = [];
+  conversation: string = ""; // contexte de conversation
 
   constructor(private chatbotService: ChatbotService) { }
 
   sendMessage() {
+    this.chatbotService.setConversation(this.conversation); // mettre à jour le contexte de conversation
     this.chatbotService.askQuestion(this.question).subscribe((response: any) => {
+      this.conversation = response.conversation; // mettre à jour le contexte de conversation avec la réponse de l'API
       this.answer = response.message;
       this.messages.push({ type: 'user', text: this.question }); // Ajout du message de l'utilisateur
       this.messages.push({ type: 'bot', text: response.message }); // Ajout de la réponse du chatbot
--- ngfront/src/app/services/chatbot.service.ts
+++ ngfront/src/app/services/chatbot.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { baseUrl } from '../models/login.model';
 
 @Injectable({
@@ -7,11 +8,24 @@ import { baseUrl } from '../models/login.model';
 })
 export class ChatbotService {
 
+  private apiUrl: string = baseUrl + 'chatbot/';
+
   constructor(private http: HttpClient) { }
 
-  askQuestion(question: string) {
-    const body = { message: question };
-    return this.http.post(baseUrl + 'chatbot/', body);
+  public askQuestion(question: string): Observable<any> {
+    return this.http.post(this.apiUrl, { question: question, conversation: this.getConversation() });
+  }
+
+  // public getWelcomeMessage(): Observable<any> {
+  //   return this.http.get(this.apiUrl);
+  // }
+
+  public setConversation(conversation: string): void {
+    localStorage.setItem('conversation', conversation);
+  }
+
+  public getConversation(): string {
+    return localStorage.getItem('conversation') || '';
   }
 
 }
